e15ad0822eb873e719ba691f40923057f18a1de4
Xaz16 <gxaz16@gmail.com>
2021-05-22 16:00:34 +0300

    initial commit

---
+++ apps/marvel-heroes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'achipiga-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'marvel-heroes';
+}
